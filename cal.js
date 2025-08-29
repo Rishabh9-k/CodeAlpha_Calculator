e1a9e33eebc30f1157daea03e858587f316c0f92
RISHABH KUMAR GHRITLAHARE <ghritlaharerishabh9@gmail.com>
2025-08-29 21:56:00 +0530

    Add files via upload

---
+++ cal.js
@@ -0,0 +1,22 @@
+function append(value) {
+    const display = document.getElementById('display');
+    display.value += value;
+  }
+  
+  function clearDisplay() {
+    document.getElementById('display').value = '';
+  }
+  
+  function deleteLast() {
+    const display = document.getElementById('display');
+    display.value = display.value.slice(0, -1);
+  }
+  
+  function calculate() {
+    try {
+      const display = document.getElementById('display');
+      display.value = eval(display.value);
+    } catch (e) {
+      alert('Invalid expression');
+    }
+  }
